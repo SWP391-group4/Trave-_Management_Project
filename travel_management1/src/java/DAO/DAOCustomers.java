9afa364a73d9a1d7cb58aab25bbeacf09398415e
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2022-06-15 00:41:23 +0700

    .

--- travel_management1/src/java/DAO/DAOCustomers.java
+++ travel_management1/src/java/DAO/DAOCustomers.java
@@ -54,6 +54,7 @@ public class DAOCustomers extends connectDB{
     }
     
     public int updateCustomer() {
+        //comment
         return 0;
     }
     public static void main(String[] args) {
