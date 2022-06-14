0bcf104431c884e896088c5a3cdd9c8e411ed033
tungnshe161905 <105336859+tungnshe161905@users.noreply.github.com>
2022-06-14 18:27:59 +0700

    change 2

--- travel_management1/src/java/DAO/DAOHomeStays.java
+++ travel_management1/src/java/DAO/DAOHomeStays.java
@@ -4,10 +4,35 @@
  */
 package DAO;
 
+import DBContext.connectDB;
+
+import Entity.HomeStays;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author phams
  */
-public class DAOHomeStays {
-    
+public class DAOHomeStays extends connectDB{
+    public HomeStays search(String homeStayID) {
+        String sql = "select * from accounts where "
+                + "homeStayID = '"+homeStayID+"'";
+        ResultSet rs = getData(sql);
+        try {
+            if(rs.next()) {
+                
+              return new HomeStays(
+                      rs.getString(1), 
+                      rs.getString(2),
+                      rs.getString(3));   
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOAccounts.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
 }
