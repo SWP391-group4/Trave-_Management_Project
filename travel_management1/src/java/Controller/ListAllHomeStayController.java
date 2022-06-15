230f3ef62a363e70dd72068c9b48969bd04ad13d
thinhnqhe150498 <105189247+thinhnqhe150498@users.noreply.github.com>
2022-06-15 11:54:27 +0700

    update

--- travel_management1/src/java/Controller/ListAllHomeStayController.java
+++ travel_management1/src/java/Controller/ListAllHomeStayController.java
@@ -4,6 +4,7 @@
  */
 package Controller;
 
+import DAO.DAOHomeStays;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -33,15 +34,8 @@ public class ListAllHomeStayController extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ListAllHomeStayController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ListAllHomeStayController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            DAOHomeStays dao = new DAOHomeStays();
+            
         }
     }
 
