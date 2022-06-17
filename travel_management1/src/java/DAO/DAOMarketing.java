12e321d0ab39c824db69b13c164690e459eb6207
tungnshe161905 <105336859+tungnshe161905@users.noreply.github.com>
2022-06-17 10:53:25 +0700

    Update DAOMarketing.java

--- travel_management1/src/java/DAO/DAOMarketing.java
+++ travel_management1/src/java/DAO/DAOMarketing.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 /**
  *
  * @author nam yeu tung and sang
+ * non tay thinh
  */
 public class DAOMarketing extends connectDB{
     public MarketingImage getMarketingImage(String accountM) {
