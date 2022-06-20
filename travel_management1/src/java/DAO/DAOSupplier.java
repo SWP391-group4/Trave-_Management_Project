315b6e26ce01a604469c6a5f2dfd5bd25d27536b
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2022-06-21 01:35:37 +0700

    daoSupplier

--- travel_management1/src/java/DAO/DAOSupplier.java
+++ travel_management1/src/java/DAO/DAOSupplier.java
@@ -6,9 +6,12 @@ package DAO;
 
 import DBContext.connectDB;
 import Entity.SupplierAddresses;
+import Entity.SupplierHomestays;
 import Entity.Suppliers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -35,6 +38,7 @@ public class DAOSupplier extends connectDB {
         }
         return null;
     }
+
     public SupplierAddresses getSupplierAddresses(String accountS) {
         String sql = "Select * from customerAddresses where accountC = '" + accountS + "'";
         ResultSet rs = getData(sql);
@@ -52,6 +56,61 @@ public class DAOSupplier extends connectDB {
         }
         return null;
     }
+
+    public List<Suppliers> listTopTenSupplier() {
+        List<Suppliers> list = new ArrayList<>();
+        String sql = "select top 10 * from suppliers";
+        ResultSet rs = getData(sql);
+        try {
+            while (rs.next()) {
+                list.add(new Suppliers(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6)
+                ));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public List<SupplierHomestays> listHomestayBySupplier() {
+        List<SupplierHomestays> list = new ArrayList<>();
+        String sql = "select s.AccountS, s.firstName,s.lastName,s.email,h.homestayName\n"
+                + "from Homestays h inner join Suppliers s on h.AccountS = s.AccountS\n"
+                + "group by s.AccountS, s.firstName,s.lastName,s.email,h.homestayName";
+        ResultSet rs = getData(sql);
+        try {
+            while (rs.next()) {
+                list.add(new SupplierHomestays(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5)
+                ));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public List<SupplierHomestays> listTop10Homestay() {
+        List<SupplierHomestays> list = listHomestayBySupplier();
+        List<SupplierHomestays> listTop = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listTop.add(list.get(i));
+        }
+        return listTop;
+    }
+
     public static void main(String[] args) {
         DAOSupplier d = new DAOSupplier();
         System.out.println(d.getSuppiler("nguyenphuong"));
