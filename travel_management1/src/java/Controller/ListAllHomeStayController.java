00a59073c5216ea229b07ef9bc8132b307d67d61
thinhnqhe150498 <105189247+thinhnqhe150498@users.noreply.github.com>
2022-06-20 22:48:19 +0700

    Update ListAllHomeStayController.java

--- travel_management1/src/java/Controller/ListAllHomeStayController.java
+++ travel_management1/src/java/Controller/ListAllHomeStayController.java
@@ -54,6 +54,7 @@ public class ListAllHomeStayController extends HttpServlet {
             List<HomeStays> listHomeStay=dao.paggingHomeStay(index);
            request.setAttribute("endPage", endPage);
             request.setAttribute("list", listHomeStay);
+            request.setAttribute("tag", index);
              request.getRequestDispatcher("/ListAll.jsp").forward(request, response);
         }
     
