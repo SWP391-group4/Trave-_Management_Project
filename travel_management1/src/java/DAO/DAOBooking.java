db3ca170f0740d274027f912bea81a30550150ed
tungnshe161905 <105336859+tungnshe161905@users.noreply.github.com>
2022-06-29 09:14:22 +0700

    Update DAOBooking.java

--- travel_management1/src/java/DAO/DAOBooking.java
+++ travel_management1/src/java/DAO/DAOBooking.java
@@ -4,10 +4,26 @@
  */
 package DAO;
 
+import DBContext.connectDB;
+import Entity.Booking;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author phams
  */
-public class DAOBooking {
-    
+public class DAOBooking extends connectDB{
+    public int addBooking(Booking book) {
+        int n = 0;
+        String sql = "insert Stores(stor_id,stor_name,stor_address,city,state,zip) "
+                + "values ('" + s.getStor_id() + "','" + s.getStor_name() + "','" + s.getStor_address() + "','" + s.getCity() + "','" + s.getStates() + "','" + s.getZip() + "')";
+        try {
+            Statement state = conn.createStatement();
+            n = state.executeUpdate(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return n;
+    }
 }
