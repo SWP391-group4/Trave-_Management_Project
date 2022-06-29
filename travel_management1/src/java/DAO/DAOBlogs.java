844a6d20656ae2f39ffc3ceceb13528ac9e16c67
tungnshe161905 <105336859+tungnshe161905@users.noreply.github.com>
2022-06-29 17:32:19 +0700

    Update DAOBlogs.java

--- travel_management1/src/java/DAO/DAOBlogs.java
+++ travel_management1/src/java/DAO/DAOBlogs.java
@@ -4,10 +4,30 @@
  */
 package DAO;
 
+import DBContext.connectDB;
+import Entity.Blogs;
+import Entity.HomeStays;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author phams
  */
-public class DAOBlogs {
+public class DAOBlogs extends connectDB {
+    public int addBlogs(Blogs bl) {
+        int n = 0;
+        String sql = "insert into HomeStays(BlogId,Image,Title) \n"
+                + "values ('" + bl.getBlogId() + "','" + bl.getImage() + "','" + bl.getTitle()+ "')";
+
+        try {
+            Statement state = conn.createStatement();
+            n = state.executeUpdate(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return n;
+    }
+    
     
 }
