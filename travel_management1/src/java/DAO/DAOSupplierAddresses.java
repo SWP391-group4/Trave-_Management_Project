d06ab75313ec1a3fd1993b4aad1feb05f0ad7a9f
tungnshe161905 <105336859+tungnshe161905@users.noreply.github.com>
2022-07-05 16:18:35 +0700

    Update DAOSupplierAddresses.java

--- travel_management1/src/java/DAO/DAOSupplierAddresses.java
+++ travel_management1/src/java/DAO/DAOSupplierAddresses.java
@@ -4,10 +4,32 @@
  */
 package DAO;
 
+import DBContext.connectDB;
+import Entity.SupplierAddresses;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author phams
  */
-public class DAOSupplierAddresses {
-    
+public class DAOSupplierAddresses extends connectDB{
+    public int addSuppliersAddresses(SupplierAddresses sup) {
+        int n = 0;
+        String sql = "insert into Suppliers(AccountS,city,district,specific,ward) \n"
+                + "values (?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement pre = conn.prepareStatement(sql);
+            pre.setString(1, sup.getAccountS());
+            pre.setString(2, sup.getCity());
+            pre.setString(3, sup.getDistrict());
+            pre.setString(4, sup.getSpecific());
+            pre.setString(5, sup.getWard());
+            n = pre.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return n;
+    }
 }
