45bef157d4e8ddd926e3d51b2e84bc8814cbba02
tungnshe161905 <105336859+tungnshe161905@users.noreply.github.com>
2022-07-06 23:52:10 +0700

    Insert Extension

---
+++ travel_management1/src/java/Controller/ExtensionsInsert.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import DAO.DAOExtensions;
+import Entity.Extensions;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "ExtensionsInsert", urlPatterns = {"/ExtensionsInsert"})
+public class ExtensionsInsert extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        response.sendRedirect("InsertExtensions.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        DAOExtensions sup = new DAOExtensions();
+
+        String HomeStayId = request.getParameter("HomeStayId");
+        String ListExtensions = request.getParameter("ListExtensions");
+        
+        //validation           
+
+        
+        int m = sup.addExtensions(new Extensions(HomeStayId, ListExtensions));
+        if (m > 0) {
+            String a = "Done";
+            request.setAttribute("alert", a);
+            request.getRequestDispatcher("InsertExtensions.jsp").forward(request, response);
+        } else {
+            String a = "Fails";
+            request.setAttribute("alert", a);
+            request.getRequestDispatcher("InsertExtensions.jsp").forward(request, response);
+
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
