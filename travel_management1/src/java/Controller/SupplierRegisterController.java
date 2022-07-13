df5edda17b38068801e6a4e6dab7f9fe7a0e85c1
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2022-07-14 00:15:50 +0700

    supplier register

--- travel_management1/src/java/Controller/SupplierRegisterController.java
+++ travel_management1/src/java/Controller/SupplierRegisterController.java
@@ -4,9 +4,9 @@ package Controller;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 import DAO.DAOSupplierTemp;
 import Entity.Accounts;
+import Entity.SupplierAddresses;
 import Entity.Suppliers;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -34,7 +35,7 @@ public class SupplierRegisterController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -65,20 +66,49 @@ public class SupplierRegisterController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-        
+
         DAOSupplierTemp daoTemp = new DAOSupplierTemp();
-        
-        Accounts account = (Accounts)request.getSession().getAttribute("acc");
+
+        Accounts account = (Accounts) request.getSession().getAttribute("acc");
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
-        String phone  = request.getParameter("phone");
+        String phone = request.getParameter("phone");
         String email = request.getParameter("email");
         String fax = request.getParameter("fax");
-        
+
         Suppliers sup = new Suppliers(account.getAccount(), firstName, lastName, fax, email, phone);
-        int n = daoTemp.addSupplier(sup);
-        
-        
+
+        String city = request.getParameter("city");
+        String district = request.getParameter("district");
+        String ward = request.getParameter("ward");
+        String specific = request.getParameter("specific");
+        SupplierAddresses supAddress = new SupplierAddresses(account.getAccount(), city, district, specific, ward);
+
+        String preview = request.getParameter("preview");
+        if (preview == null) {
+            int n = daoTemp.addSupplier(sup);
+            int m = daoTemp.addSupplierAddress(supAddress);
+            if (m != 0 && n != 0) {
+                String alert = "Register done.";
+                request.setAttribute("alert", alert);
+                request.getRequestDispatcher("RegisterSupplier.jsp").forward(request, response);
+            }
+        } else {
+            System.out.println(sup);
+            System.out.println(supAddress);
+            request.setAttribute("firstName", firstName);
+            request.setAttribute("lastName", lastName);
+            request.setAttribute("phone", phone);
+            request.setAttribute("email", email);
+            request.setAttribute("fax", fax);
+
+            request.setAttribute("city", city);
+            request.setAttribute("district", district);
+            request.setAttribute("ward", ward);
+            request.setAttribute("specific", specific);
+
+            request.getRequestDispatcher("RegisterSupplier.jsp").forward(request, response);
+        }
     }
 
     /**
